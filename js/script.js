fb9151bc0bd6d202cc8f3a3247614d547429e3aa
Aman Sharma <halfengiiineeer2022@gmail.com>
2023-03-19 21:15:18 +0530

    first

--- js/script.js
+++ js/script.js
@@ -9,58 +9,62 @@ let start = document.querySelector('.start')
 
 
 let result = 0
-let currentTime = 60
+let currentTime = 10
 let hitPos
 var time
 
 
 start.addEventListener('click' , () => {
-    function randomSquare() {
-        squares.forEach((square) => {
-            square.classList.remove('mole')
-        })
-    
-        let randomPosition = squares[Math.floor(Math.random() * 9)]
-        console.log(randomPosition);
-        randomPosition.classList.add('mole')
-    
-        hitPos = randomPosition.id
-    }
-    squares.forEach((squares) => {
-        squares.addEventListener('click', () => {
-            if (squares.id == hitPos) {
-                result++
-                score.textContent = `${ result }`
-                hitPos = null
-            }
-        })
-    })
-    
-    
-    function moveMole() {
-        timerId = setInterval(randomSquare, 800)
-    }
-    moveMole()
-    
-    
-    function countDown() {
-        currentTime--
-        timeLeft.textContent = currentTime
-    
-        if (currentTime == 0) {
-            clearInterval(time)
-            clearInterval(timerId)
-            res.textContent = (`Game Over !!!  Your Result is ` + result)
-            res.style.color = "white"
-            alert('Game Over !!! Your Final Score is ' + result)
-    
+    if(currentTime > 0){
+        function randomSquare() {
+            squares.forEach((square) => {
+                square.classList.remove('mole')
+            })
+        
+            let randomPosition = squares[Math.floor(Math.random() * 9)]
+            console.log(randomPosition);
+            randomPosition.classList.add('mole')
+        
+            hitPos = randomPosition.id
         }
+        squares.forEach((squares) => {
+            squares.addEventListener('click', () => {
+                if (squares.id == hitPos) {
+                    result++
+                    score.textContent = `${ result }`
+                    hitPos = null
+                }
+            })
+        })
+        
+        
+        function moveMole() {
+            timerId = setInterval(randomSquare, 500)
+        }
+        moveMole()
+        
+        
+        function countDown() {
+            currentTime--
+            timeLeft.textContent = currentTime
+        
+            if (currentTime == 0) {
+                clearInterval(time)
+                clearInterval(timerId)
+                res.textContent = (`Game Over !!!  Your Result is ` + result)
+                res.style.color = "white"
+                alert('Game Over !!! Your Final Score is ' + result)
+        
+            }
+        }
+         time = setInterval(countDown, 1000)
     }
-     time = setInterval(countDown, 1000)
 })
 
 
 stop.addEventListener('click' , () => {
-        clearInterval(timerId)
-        clearInterval(time)
+        if(currentTime > 0){
+            clearInterval(time)
+            clearInterval(timerId)
+        }
 })
